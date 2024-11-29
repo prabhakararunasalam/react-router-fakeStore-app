9668756217dc15fac90279d67b2c1ac87d4afdb9
prabhakararunasalam <prabhakararunasalam17@gmail.com>
2024-11-29 17:40:00 +0530

    completed

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,18 +1,32 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const Cart = ({ cart, updateCart,updateQuantity }) => {
+const Cart = ({ cart, updateCart, updateQuantity }) => {
   const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
   const discountedTotal = total * 0.9;
 
+  const navigate = useNavigate();
+
   return (
     <div className="p-6 absolute top-[100px] left-[20%]">
-      <h2 className="text-2xl font-bold m-5">Cart</h2>
+      <div className="flex justify-between items-center ">
+        <h2 className="text-2xl font-bold m-5">Cart</h2>
+
+        <button onClick={() =>navigate("/") } className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+            Home
+          </span>
+        </button>
+      </div>
       {cart.length === 0 ? (
         <p>Your cart is empty!</p>
       ) : (
         <div>
           {cart.map((item) => (
-            <div key={item.id} className="flex justify-between items-center border p-4 rounded gap-5 m-5">
+            <div
+              key={item.id}
+              className="flex justify-between items-center border p-4 rounded gap-5 m-5"
+            >
               <img className="h-12 m-3 " src={item.image} alt={item.title} />
               <p className="m-3 font-bold">{item.title}</p>
               <p>${item.price}</p>
@@ -40,7 +54,9 @@ const Cart = ({ cart, updateCart,updateQuantity }) => {
             </div>
           ))}
           <h3 className="text-lg font-bold mt-4">Total: ${total.toFixed(2)}</h3>
-          <h3 className="text-lg font-bold">Discounted Total: ${discountedTotal.toFixed(2)}</h3>
+          <h3 className="text-lg font-bold">
+            Discounted Total: ${discountedTotal.toFixed(2)}
+          </h3>
         </div>
       )}
     </div>
